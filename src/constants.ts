cc308512d1c050de2c1911c17bfa5554fc88b54e
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2020-10-20 15:33:04 +0200

    Update factory address

--- src/constants.ts
+++ src/constants.ts
@@ -19,8 +19,8 @@ export enum Rounding {
   ROUND_UP
 }
 
-// Testnet address: 0x6FF7B2CaFb40Cc5FA00a2AAB801d5207dA88cB0C - deployed 2020-10-20 12:42 UTC
-export const FACTORY_ADDRESS = '0x6FF7B2CaFb40Cc5FA00a2AAB801d5207dA88cB0C'
+// Testnet address: 0xEea40D20dfEdBA391C71153F9f6d029269E6264d - deployed 2020-10-20 13:31 UTC
+export const FACTORY_ADDRESS = '0xEea40D20dfEdBA391C71153F9f6d029269E6264d'
 
 // Original Uniswap code hash: 0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f
 // INIT_CODE_HASH derived from @harmony-swoop/core/build/contracts/UniswapV2Pair.json bytecode -> keccak256(['bytes'], [`0x${bytecode}`])
